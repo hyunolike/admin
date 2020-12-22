8b87887a61211dcace7c4ddc784403b481a1258a
hyunolike <fallear1@gmail.com>
2020-12-23 02:22:35 +0900

    update : test crud

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,10 +4,17 @@ package com.example.study.repository;
 import com.example.study.StudyApplication;
 import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
+import com.sun.tools.javac.util.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -32,15 +39,46 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
     }
 
+    @Test
     public void read(){
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            System.out.println("user: " + selectUser);
+            System.out.println("email: " + selectUser.getEmail());
+        });
 
     }
 
+    @Test
+    @Transactional
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("pppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
     }
 
+
+    @Test
+    @Transactional //db에 있는 data가 실제로 제거 되지는 않는다 Rolled back transaction
     public void delete(){
+        Optional<User> user = userRepository.findById(3L);
+
+        Assertions.assertTrue(user.isPresent()); //true
+
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(3L);
+
+        Assertions.assertFalse(deleteUser.isPresent()); //False
 
     }
 }
